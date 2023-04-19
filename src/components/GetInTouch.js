4ca8ce6b988e60c966be837a259235a19c3960b8
rezzak ali <rezzak ali>
2023-04-19 19:27:44 +0530

    step-3 code push

--- src/components/GetInTouch.js
+++ src/components/GetInTouch.js
@@ -1,14 +1,14 @@
 function GetInTouch() {
   return (
     <div className="mt-20 mx-auto rounded-lg bg-[#eeeff259]  dark:bg-[#34384270] p-10 border dark:border-gray-800">
-      <h1 className="text-4xl font-bold text-center dark:text-white">
+      <h1 className="text-4xl font-bold text-center">
         Describe your project & requirements
       </h1>
-      <p className="text-center text-xl mt-7 dark:text-[#e2e8f0]">
+      <p className="text-center text-xl mt-7">
         Let's create your beautiful websites & digital presence 🌈
       </p>
       <div className="flex flex-col items-center mt-7">
-        <button className="rounded bg-[#0f172a] hover:bg-[#0f172ae3] dark:bg-gray-200 dark:hover:bg-gray-300 dark:text-black text-white p-2 w-[150px]">
+        <button className="rounded border border-gray-500 dark:bg-gray-200 dark:hover:bg-gray-300 dark:text-black p-2 w-[150px]">
           Get in touch
         </button>
       </div>
