d76af30076d02ef6409b16eebfb8e73d2c329ab8
rezzak ali <rezzak ali>
2023-05-25 17:24:51 +0530

    hero section description updated

--- src/utils/Description.js
+++ src/utils/Description.js
@@ -1,10 +1,11 @@
 function Description() {
   return (
     <p className="mt-7 leading-8 mx-auto p-1">
-      As a Web Developer, I specialize in creating contemporary, resilient, and
-      inclusive web user interfaces and design. My emphasis on responsive
-      design, performance and accessibility ensures that the products I create
-      are user-friendly and accessible to all.
+      A passionate web application developer from India, dedicated to crafting
+      exceptional digital experiences through code. With a strong focus on
+      creating outstanding projects, I immerse myself in the world of
+      development, constantly honing my skills to deliver cutting-edge
+      solutions.
     </p>
   );
 }
